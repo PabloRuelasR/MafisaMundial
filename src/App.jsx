2f36cc9ac07699ca1c68cdb29ac51d6c542d7abb
Pablo Ruelas <pabloruelas26@gmail.com>
2026-06-04 01:04:34 -0500

    conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -21,72 +21,6 @@ import AdminPanel from './components/AdminPanel';
 import MyPredictionsModal from './components/MyPredictionsModal';
 import './styles/animations.css';
 
-<<<<<<< HEAD
-=======
-// --- DATOS ---
-const participantesIniciales = [
-  {
-    id: 1,
-    nombre: 'Anthony',
-    puntos: 165,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
-    predicciones: [
-      {
-        eq1: 'BRA',
-        flag1: 'br',
-        eq2: 'ARG',
-        flag2: 'ar',
-        pronostico: '2-1',
-        resultado: '2-1',
-        acierto: true,
-        puntosGanados: 15
-      },
-    ]
-  },
-  {
-    id: 2,
-    nombre: 'Frank',
-    puntos: 145,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png',
-    predicciones: []
-  },
-  {
-    id: 3,
-    nombre: 'Hector',
-    puntos: 130,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png',
-    predicciones: []
-  },
-  {
-    id: 4,
-    nombre: 'Miguel',
-    puntos: 110,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/59.png',
-    predicciones: []
-  },
-  {
-    id: 5,
-    nombre: 'Jhoan',
-    puntos: 95,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png',
-    predicciones: []
-  },
-  {
-    id: 6,
-    nombre: 'Juan',
-    puntos: 80,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png',
-    predicciones: []
-  },
-  {
-    id: 7,
-    nombre: 'Pablo',
-    puntos: 152,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png',
-    predicciones: []
-  },
-];
->>>>>>> 40932ca7546d41af7257b39f00a902320e4187fe
 
 
 export default function App() {
@@ -118,7 +52,6 @@ export default function App() {
     return () => unsubscribe();
   }, []);
 
-<<<<<<< HEAD
   useEffect(() => {
     const cargarParticipantes = async () => {
       try {
@@ -160,45 +93,6 @@ export default function App() {
   if (!currentUser) {
     return <LoginScreen onLoginComplete={(profile) => setCurrentUser(profile)} />;
   }
-=======
-  const paisesMundial = [
-    {
-      nombre: 'Argentina',
-      colores: ['#43A1D5', '#ffffff', '#F6B40E'],
-      flag: 'ar'
-    },
-    {
-      nombre: 'Francia',
-      colores: ['#002395', '#ffffff', '#ED2939'],
-      flag: 'fr'
-    },
-    {
-      nombre: 'España',
-      colores: ['#AA151B', '#F1BF00', '#AA151B'],
-      flag: 'es'
-    },
-    {
-      nombre: 'Colombia',
-      colores: ['#FCD116', '#0038A8', '#CE1126'],
-      flag: 'co'
-    },
-    {
-      nombre: 'Alemania',
-      colores: ['#000000', '#FF0000', '#FFCC00'],
-      flag: 'de'
-    },
-    {
-      nombre: 'Japón',
-      colores: ['#ffffff', '#BC002D', '#ffffff'],
-      flag: 'jp'
-    },
-    {
-      nombre: 'Portugal',
-      colores: ['#BC002D', '#0b5c12'],
-      flag: 'pt'
-    }
-  ];
->>>>>>> 40932ca7546d41af7257b39f00a902320e4187fe
 
   const activarBocina = () => {
     const pais =
