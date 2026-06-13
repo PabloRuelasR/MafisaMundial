308e1176442bffb01d07908ff2aa4b6650e9f8d8
PabloR <87242322+PabloRuelasR@users.noreply.github.com>
2026-06-13 12:30:52 -0500

    CAMBIOS

--- src/components/PredictionPanel.jsx
+++ src/components/PredictionPanel.jsx
@@ -7,7 +7,7 @@ const getPeruDate = () => {
     const date = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Lima" }));
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0');
-    const dd = String(date.getDate()).padStart(2, '0');
+    const dd = String(date.getDate() - 1).padStart(2, '0');
     return `${yyyy}-${mm}-${dd}`;
 };
 
